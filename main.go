a54d2fef829cf3a46cc4d75e3dbe9d17898951a3
alex-b23 <138300434+alex-b23@users.noreply.github.com>
2023-11-03 19:09:28 +0000

    Had to delete old repo due to issues

---
+++ main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"database/sql"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/gorilla/mux"
+)
+
+// the structure of the player filesystem
+type Player struct {
+	ID 			   int 		`json:"id"`
+	PlayerUsername string 	`json:"player_username"`
+	PlayerPassword string 	`json:"player_password"`
+	PlayerWins 	   int 		`json:"player_wins"`
+	PlayerLoses    int 		`json:"player_loses"`
+	PlayerTotalGame int 	`json:"player_total_game"`
+}
+
+func main() {
+	// Connecting to SQL Database
+	db, err := sql.Open("postgres", os.Getenv("DATABASE_URL")) // TO DO -> Change URL once Docker Setup
+
+	if err != nil {
+		log.Fatal(err)
+	}
+	// Close DB Connection
+	defer db.Close()
+
+	// Creating Router Requests
+	route := mux.NewRouter()
+	route.HandleFunc("/players", getPlayer(db)).Methods("GET") // getPlayer -> used to get players from database
+	route.HandleFunc("/players/{id}", getPlayerByID(db)).Methods("GET") // getPlayerByID -> used to get player by ID
+	route.HandleFunc("/players", createPlayer(db)).Methods("POST") // createPlayer -> add a new player to the database
+	route.HandleFunc("/players/{id}", updatePlayer(db)).Methods("PUT") // updateUser -> update player information
+	route.HandleFunc("/players/{id}", deletePlayer(db)).Methods("DELETE") // deletePlayer -> delete player from database
+
+	// used to start the server
+	log.Fatal(http.ListenAndServe(":3000", route))
+}
