46580e7bc1dc01fcfdaa92ed19634c8aaca2abee
alex-b23 <138300434+alex-b23@users.noreply.github.com>
2023-11-03 20:23:20 +0000

    createPlayer ,updatePlayer, deletePlayer created

--- main.go
+++ main.go
@@ -10,18 +10,18 @@ import (
 )
 
 func main() {
-    db, err := sql.Open("postgres", os.Getenv("DATABASE_URL"))
+    db, err := sql.Open("postgres", os.Getenv("DATABASE_URL")) // TO DO -> Connect Database Once Setup
     if err != nil {
         log.Fatal(err)
     }
     defer db.Close()
 
     route := mux.NewRouter()
-    route.HandleFunc("/players", getPlayer(db)).Methods("GET")
-    route.HandleFunc("/players/{id}", getPlayerByID(db)).Methods("GET")
-    route.HandleFunc("/players", createPlayer(db)).Methods("POST")
-    route.HandleFunc("/players/{id}", updatePlayer(db)).Methods("PUT")
-    route.HandleFunc("/players/{id}", deletePlayer(db)).Methods("DELETE")
+    route.HandleFunc("/players", getPlayer(db)).Methods("GET") // -> method used to get all players
+    route.HandleFunc("/players/{id}", getPlayerByID(db)).Methods("GET") // -> method used to get player by id
+    route.HandleFunc("/players", createPlayer(db)).Methods("POST") // -> method used to create new player
+    route.HandleFunc("/players/{id}", updatePlayer(db)).Methods("PUT") // -> method used to update player by id
+    route.HandleFunc("/players/{id}", deletePlayer(db)).Methods("DELETE") // -> method used to delete player by id
 
     log.Fatal(http.ListenAndServe(":3000", jsonMiddleware(route)))
 }
--- player.go
+++ player.go
@@ -4,6 +4,8 @@ import (
 	"database/sql"
 	"encoding/json"
 	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
 type Player struct {
@@ -47,18 +49,116 @@ func getPlayer(db *sql.DB) http.HandlerFunc {
     }
 }
 
+// this function will get player by the specified id
 func getPlayerByID(db *sql.DB) http.HandlerFunc {
-    // implementation here
+    return func(w http.ResponseWriter, r *http.Request) {
+        vars := mux.Vars(r)
+        id, ok := vars["id"]
+        if !ok {
+            http.Error(w, "Missing URL parameter id", http.StatusBadRequest)
+            return
+        }
+
+        var u Player
+        err := db.QueryRow("SELECT * FROM players WHERE id = $1", id).Scan(&u.ID, &u.PlayerUsername, &u.PlayerPassword, &u.PlayerWins, &u.PlayerLoses, &u.PlayerTotalGame)
+        if err == sql.ErrNoRows {
+            http.Error(w, "No player with that ID", http.StatusNotFound)
+            return
+        } else if err != nil {
+            http.Error(w, err.Error(), http.StatusInternalServerError)
+            return
+        }
+
+        w.Header().Set("Content-Type", "application/json")
+        w.WriteHeader(http.StatusOK)
+        if err := json.NewEncoder(w).Encode(u); err != nil {
+            http.Error(w, err.Error(), http.StatusInternalServerError)
+        }
+    }
 }
 
+// This function creates a new player to the database
 func createPlayer(db *sql.DB) http.HandlerFunc {
-    // implementation here
+    return func(w http.ResponseWriter, r *http.Request) {
+        var u Player
+        if err := json.NewDecoder(r.Body).Decode(&u); err != nil {
+            http.Error(w, err.Error(), http.StatusBadRequest)
+            return
+        }
+
+        sqlStatement := `
+        INSERT INTO players (player_username, player_password, player_wins, player_loses, player_total_game)
+        VALUES ($1, $2, $3, $4, $5)
+        RETURNING id`
+        id := 0
+        err := db.QueryRow(sqlStatement, u.PlayerUsername, u.PlayerPassword, u.PlayerWins, u.PlayerLoses, u.PlayerTotalGame).Scan(&id)
+        if err != nil {
+            http.Error(w, err.Error(), http.StatusInternalServerError)
+            return
+        }
+
+        w.Header().Set("Content-Type", "application/json")
+        w.WriteHeader(http.StatusCreated)
+        if err := json.NewEncoder(w).Encode(id); err != nil {
+            http.Error(w, err.Error(), http.StatusInternalServerError)
+        }
+    }
 }
 
+// this function updates player
 func updatePlayer(db *sql.DB) http.HandlerFunc {
-    // implementation here
+    return func(w http.ResponseWriter, r *http.Request) {
+        vars := mux.Vars(r)
+        id, ok := vars["id"]
+        if !ok {
+            http.Error(w, "Missing URL parameter id", http.StatusBadRequest)
+            return
+        }
+
+        var u Player
+        if err := json.NewDecoder(r.Body).Decode(&u); err != nil {
+            http.Error(w, err.Error(), http.StatusBadRequest)
+            return
+        }
+
+        sqlStatement := `
+        UPDATE players 
+        SET player_username = $1, player_password = $2, player_wins = $3, player_loses = $4, player_total_game = $5
+        WHERE id = $6
+        RETURNING id, player_username, player_password, player_wins, player_loses, player_total_game`
+        err := db.QueryRow(sqlStatement, u.PlayerUsername, u.PlayerPassword, u.PlayerWins, u.PlayerLoses, u.PlayerTotalGame, id).Scan(&u.ID, &u.PlayerUsername, &u.PlayerPassword, &u.PlayerWins, &u.PlayerLoses, &u.PlayerTotalGame)
+        if err == sql.ErrNoRows {
+            http.Error(w, "No player with that ID", http.StatusNotFound)
+            return
+        } else if err != nil {
+            http.Error(w, err.Error(), http.StatusInternalServerError)
+            return
+        }
+
+        w.Header().Set("Content-Type", "application/json")
+        w.WriteHeader(http.StatusOK)
+        if err := json.NewEncoder(w).Encode(u); err != nil {
+            http.Error(w, err.Error(), http.StatusInternalServerError)
+        }
+    }
 }
 
 func deletePlayer(db *sql.DB) http.HandlerFunc {
-    // implementation here
+    return func(w http.ResponseWriter, r *http.Request) {
+        vars := mux.Vars(r)
+        id, ok := vars["id"]
+        if !ok {
+            http.Error(w, "Missing URL parameter id", http.StatusBadRequest)
+            return
+        }
+
+        sqlStatement := `DELETE FROM players WHERE id = $1`
+        _, err := db.Exec(sqlStatement, id)
+        if err != nil {
+            http.Error(w, err.Error(), http.StatusInternalServerError)
+            return
+        }
+
+        w.WriteHeader(http.StatusNoContent)
+    }
 }
